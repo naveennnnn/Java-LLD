cab3f34c7475baaf1ed22969d2afa446877e8709
Naveen <naveen.b@idp.com>
2025-11-30 23:08:06 +0530

    code structure started

---
+++ Machine_Coding/Tic_Tac_Toe/Controller/GameController.java
@@ -0,0 +1,9 @@
+package Machine_Coding.Tic_Tac_Toe.Controller;
+
+public class GameController {
+    /*
+        Will act as Entry point for the game.
+        Anyone will interact with the game using this controller. THis is analogous to Waiter
+    
+    */
+}
