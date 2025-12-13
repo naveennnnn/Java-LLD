4f67a76d95a77970b9f26982239a37117f06b68a
Naveen <naveen.b@idp.com>
2025-12-13 23:51:33 +0530

    Update Main.java

--- Machine_Coding/ParkingLot/Main.java
+++ Machine_Coding/ParkingLot/Main.java
@@ -36,5 +36,6 @@ public class Main {
 
         System.out.println("Gate Id: "+ticket.getGateId());
         System.out.println("Entry time :"+ticket.getEntryTime());
+        System.out.println("ticket ID: "+ticket.getId());
     }
 }
